bf2a0abe60b8222900a9e7036b56ad437c35841c
mcvollenweider <mcvollenweider@gmail.com>
2022-12-30 10:24:19 -0500

    added more middleware (morgan)

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ console.log("starting up");
 const express = require("express");
 const app = express();
 
+const morgan = require('morgan');
+app.use(morgan('dev'))
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
@@ -12,13 +17,35 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
   res.send([
     {
-      name: "robert",
+      name: "tom",
     },
     {
         name: "christian",
       },
   ]);
 });
+
+
+app.post('/users', (req, res) => {
+  console.log('POST request made by users');
+  console.log('request object:', req)
+  console.log('body:', req.body)
+  // do something with the body, then send back a response
+  res.send({message: 'nothing here yet', youSentUs: req.body})
+})
+
+//***this code in console*/
+// fetch('/users', {
+//   method: 'POST',
+//   headers: {
+//     'Content-type': 'Application/json',
+//   },
+//   body: JSON.stringify([
+//     {name: 'Mark'},
+//     {name: 'Christian'},
+//   ])
+// }).then(res => 
+// res.json()).then(console.log).catch(console.log);
 
 const PORT = 3000;
 
